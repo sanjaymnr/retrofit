b7d0266c228c1a1a2ecc06214e591b160bef1164
sanjaymnr <tamata.sanjay@gmail.com>
2019-02-07 16:53:05 +0545

    added printing new line

--- app/src/main/java/com/example/user/retrofit/MainActivity.java
+++ app/src/main/java/com/example/user/retrofit/MainActivity.java
@@ -4,15 +4,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 import okhttp3.OkHttpClient;
-import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,11 +19,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Call<List<Modal>> questionModelCall=ApiService.getServiceClass().getAllJavaQuestions();
+        Call<List<Modal>> questionModelCall = ApiService.getServiceClass().getAllJavaQuestions();
         questionModelCall.enqueue(new Callback<List<Modal>>() {
             @Override
             public void onResponse(Call<List<Modal>> call, Response<List<Modal>> response) {
+                if (response.isSuccessful()) {
+                    for (int i = 0; i < response.body().size(); i++) {
+                        String category = response.body().get(i).category;
+                        String instructions = response.body().get(i).instructions;
+                        String name = response.body().get(i).name;
+                        String photo = response.body().get(i).photo;
+                        Double price = response.body().get(i).price;
+                        Integer productId = response.body().get(i).productId;
+                        System.out.println("productID...." + productId + "...name... " + name + "...category..." + category
+                                + "...instructions..." + instructions + "...photo..." + photo + "...price..." + price);
+                    }
 
+                }
             }
 
             @Override
